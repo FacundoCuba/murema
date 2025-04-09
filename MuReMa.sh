c6c517141f2c99130931b19eed7e000e635548a4
Facundo Cuba <facundogcuba@gmail.com>
2025-04-09 14:28:13 -0300

    Update MuReMa.sh
    Mayor update, change input from sample list to directly paired reads. Added functionalities: read trimming, consensus generation and log improvements.

--- MuReMa.sh
+++ MuReMa.sh
@@ -2,194 +2,210 @@
 
 # Function to display script usage
 function display_usage() {
-    echo "Usage: $0 -s FILE -d FILE [-r INTEGER] [-t INTEGER] [-h]"
+    echo "Usage: $0 -1 FASTQ -2 FASTQ -d FILE [-b FILE] [-r INTEGER] [-t INTEGER] [-T|-U] [-v] [-h]"
     echo ""
     echo "Options:"
-    echo "  -s FILE     Samples file OR path to the samples file"
-    echo "  -d FILE     Multifasta file OR path to the multifasta file"
-    echo "  -r INTEGER  Read length (default: 150)"
-    echo "  -t INTEGER  Average Depth threshold (default: 1000)"
-    echo "  -h          Display this help message"
+    echo "  -1 FASTQ   Forward read OR path to forward read"
+    echo "  -2 FASTQ   Reverse read OR path to reverse read"
+    echo "  -d FILE    Multifasta file OR path to the multifasta file"
+    echo "  -b FILE    Bed file OR path to bed file (optional)"
+    echo "  -r INTEGER Read length (default: 150)"
+    echo "  -t INTEGER Average Depth threshold (default: 1000)"
+    echo "  -T         Specify that the provided reads are already trimmed"
+    echo "  -U         Specify that the provided reads are untrimmed (default)"
+    echo "  -v         Display the version of the script"
+    echo "  -h         Display this help message"
+    exit 1
+}
+
+# Display version
+function display_version() {
+    echo "Script Version: 3.0"
     exit 0
 }
 
 # Initialize variables with default values
-samples_file=""
 multifasta_file=""
+bed_file=""
 read_length=150
 avg_depth_threshold=1000
+sample_name=""
+log_file=""
+version="3.0"
+trim_reads=true  # Default behavior is to trim reads
 
 # Parse options using getopts
-while getopts "s:d:r:t:h" option; do
+while getopts "1:2:d:b:r:t:TUvh" option; do
     case "$option" in
-        s) samples_file=$PWD/$OPTARG;;
-        d) multifasta_file=$PWD/$OPTARG;;
-        r) read_length=$OPTARG;;
-        t) avg_depth_threshold=$OPTARG;;
+        1) r1="$PWD/$OPTARG";;
+        2) r2="$PWD/$OPTARG";;
+        d) multifasta_file="$PWD/$OPTARG";;
+        b) bed_file="$PWD/$OPTARG";;
+        r) read_length="$OPTARG";;
+        t) avg_depth_threshold="$OPTARG";;
+        T) trim_reads=false;;
+        U) trim_reads=true;;
+        v) display_version;;
         h) display_usage;;
-        :) printf "missing argument for -%s\n" "$OPTARG" >&2; display_usage >&2; exit 1;;
-        \?) printf "illegal option: -%s\n" "$OPTARG" >&2; display_usage >&2; exit 1;;
+        :) printf "Missing argument for -%s\n" "$OPTARG" >&2; display_usage >&2;;
+        \?) printf "Illegal option: -%s\n" "$OPTARG" >&2; display_usage >&2;;
     esac
 done
 
 # Check if required arguments are provided
-if [ -z "$samples_file" ] || [ -z "$multifasta_file" ]; then
-    echo "Error: At least arguments -s and -d must be provided."
+if [[ -z "$r1" || -z "$r2" || -z "$multifasta_file" ]]; then
+    echo "Error: Arguments -1, -2, and -d must be provided." >&2 | tee -a "$log_file"
     display_usage
 fi
 
-# Log file
-log_file="${samples_file}.log"
+# Set sample name
+if [[ $trim_reads == true ]]; then
+    sample_path="${r1%_S*_R1_001.fastq.gz}"
+    sample_name=$(basename "$sample_path")
+else
+    sample_path="${r1%_*1.fastq.gz}"
+    sample_name=$(basename "$sample_path")
+fi
 
-# Log the initial prompt to the log file
+# Create sample directory and log file
+mkdir -p "$sample_name"
+cd "$sample_name"
+log_file="${sample_name}.log"
+# Log initial arguments
 {
     echo "Script invoked with the following arguments:"
-    echo "Samples file: $samples_file"
+    echo "Sample name: $sample_name"
+    echo "Forward read: $r1"
+    echo "Reverse read: $r2"
     echo "Multifasta DB: $multifasta_file"
+    echo "Bed file: $bed_file"
     echo "Read length: $read_length"
     echo "Average Depth threshold: $avg_depth_threshold"
+    echo "Trimming reads: $trim_reads"
 } > "$log_file"
+cd ../
 
 # Inputs validation
-if [ ! -e "$samples_file" ]; then
-    echo "Error: $samples_file is not a valid file." | tee -a "$log_file"
-    exit 1
-fi
-
-if [ ! -e "$multifasta_file" ]; then
-    echo "Error: $multifasta_file is not a valid file." | tee -a "$log_file"
-    exit 1
-fi
+for file in "$r1" "$r2" "$multifasta_file"; do
+    if [[ ! -e "$file" ]]; then
+        echo "Error: $file is not a valid file." >&2 | tee -a "$log_file"
+        exit 1
+    fi
+done
 
 if ! [[ "$read_length" =~ ^[1-9][0-9]*$ ]]; then
-    echo "Error: $read_length is not a valid integer greater than 0." | tee -a "$log_file"
+    echo "Error: $read_length is not a valid integer greater than 0." >&2 | tee -a "$log_file"
     exit 1
 fi
 
 if ! [[ "$avg_depth_threshold" =~ ^[1-9][0-9]*$ ]]; then
-    echo "Error: $avg_depth_threshold is not a valid integer greater than 0." | tee -a "$log_file"
+    echo "Error: $avg_depth_threshold is not a valid integer greater than 0." >&2 | tee -a "$log_file"
     exit 1
 fi
 
-# Ensure required tools are installed
-{
-    command -v bowtie2 >/dev/null 2>&1 || { echo >&2 "bowtie2 is required but it's not installed. Aborting."; exit 1; }
-    command -v samtools >/dev/null 2>&1 || { echo >&2 "samtools is required but it's not installed. Aborting."; exit 1; }
-    command -v python3 >/dev/null 2>&1 || { echo >&2 "python3 is required but it's not installed. Aborting."; exit 1; }
-    command -v ivar >/dev/null 2>&1 || { echo >&2 "ivar is required but it's not installed. Aborting."; exit 1; }
-} | tee -a "$log_file"
+# Check for required tools
+for tool in trim_galore bowtie2 samtools python3 ivar; do
+    command -v $tool >/dev/null 2>&1 || { echo "$tool is required but it's not installed. Aborting." >&2 | tee -a "$log_file"; exit 1; }
+done
 
-# Make the Python scripts executable (if not already)
-chmod +x formater.py
-chmod +x grapher.py
+# Ensure Python scripts are executable
+chmod +x ../formater.py ../grapher.py
 
 # Create DB directory and index the multifasta
 mkdir -p DB_dir
 cp "$multifasta_file" DB_dir/murema_DB.fasta
 cd DB_dir
+
 if [ ! -f "murema_DB_index.1.bt2" ]; then
-    bowtie2-build -f murema_DB.fasta murema_DB_index
+    bowtie2-build -f murema_DB.fasta murema_DB_index || { echo "Error: bowtie2-build failed." | tee -a "$log_file"; exit 1; }
 fi
 cd ../
 
-# Process each sample
-while IFS= read -r sample_name; do
-    mkdir -p "$sample_name"
+# Check if reads need trimming
+if [[ $trim_reads == true ]]; then
+    # Perform trimming
     cd "$sample_name"
-    {    
-        echo ""
-        echo "Processing Sample: $sample_name"
-    } | tee -a "$log_file"
+    echo "Trimming reads for sample $sample_name" | tee -a "$log_file"
+    trim_galore -j 8 -q 30 --paired --length 100 -o ./ --no_report_file "$r1" "$r2" --basename "$sample_name"
+    mv "${sample_name}_val_1.fq.gz" "${sample_name}_1.fastq.gz"
+    mv "${sample_name}_val_2.fq.gz" "${sample_name}_2.fastq.gz"
+    r1_trimmed="${sample_name}_1.fastq.gz"
+    r2_trimmed="${sample_name}_2.fastq.gz"
+    cd ../
+else
+    # Skip trimming
+    cd "$sample_name"
+    echo "Skipping trimming step as reads are already trimmed." | tee -a "$log_file"
+    r1_trimmed="$r1"
+    r2_trimmed="$r2"
+    cd ../
+fi
 
-    # Align reads and sort bam
-    bowtie2 --end-to-end --very-sensitive -x "../DB_dir/murema_DB_index" -1 "../${sample_name}_1.fastq.gz" -2 "../${sample_name}_2.fastq.gz" | samtools sort | samtools view -@ 8 -b -F 4 -q 1 -o "${sample_name}.sorted.bam"
-    if [ $? -ne 0 ]; then
-        {
-            echo ""
-            echo "Error: bowtie2 or samtools sorting failed for sample $sample_name"
-        } | tee -a "$log_file"
-        cd ../
-        continue
-    fi
+# Align reads and sort BAM
+cd "$sample_name"
+echo "Aligning reads and generating sorted BAM for $sample_name" | tee -a "$log_file"
+bowtie2 --end-to-end --very-sensitive -p 8 -x "../DB_dir/murema_DB_index" -1 "$r1_trimmed" -2 "$r2_trimmed" | \
+samtools sort | samtools view -@ 8 -b -F 4 -q 1 -o "${sample_name}.sorted.bam" || { echo "Error: bowtie2 or samtools sorting failed" | tee -a "$log_file"; exit 1; }
 
-    # Index sorted BAM file and generate a summary TSV file for the sample
+# Check if optional bed_file exists
+if [ -n "$bed_file" ] && [ -f "$bed_file" ]; then
+    echo "Trimming primers using bed file: $bed_file" | tee -a "$log_file"
+    samtools index "${sample_name}.sorted.bam"
+    ivar trim -e -i "${sample_name}.sorted.bam" -b "${bed_file}" -p "${sample_name}.primertrim"
+    samtools sort "${sample_name}.primertrim.bam" -o "${sample_name}.primertrim.sorted.bam"
+    samtools index "${sample_name}.primertrim.sorted.bam"
+    samtools idxstats "${sample_name}.primertrim.sorted.bam" > "${sample_name}.tsv"
+else
+    echo "No bed file provided or file not found; skipping primer trimming." | tee -a "$log_file"
     samtools index "${sample_name}.sorted.bam"
     samtools idxstats "${sample_name}.sorted.bam" > "${sample_name}.tsv"
+fi
 
-    # Run formater.py script to filter and format the TSV files
-    python3 ../formater.py "${sample_name}.tsv" "$sample_name" "$read_length" "$avg_depth_threshold"
-    if [ $? -ne 0 ]; then
-        {
-            echo ""
-            echo "Error: formater.py failed for sample $sample_name"
-        } | tee -a "$log_file"
-        cd ../
-        continue
-    fi
-    sed -i 's/\r$//' "${sample_name}.filtered.tsv"
-    sed -i 's/\r$//' "${sample_name}.refs.tsv"
-    {
-        echo ""
-        echo "Finished creating filtered and refs reports for $sample_name"
-    } | tee -a "$log_file"
-    cd ../
-done < "$samples_file"
+# Run formater.py
+formater.py "${sample_name}.tsv" "$sample_name" "$read_length" "$avg_depth_threshold" || { echo "formater.py failed" | tee -a "$log_file"; exit 1; }
 
-# Prepare refs to create the consensus and graphs
-cd DB_dir/
-cat ../*/*.refs.tsv > all_refs.tsv
-uniq all_refs.tsv > uniq_refs.tsv
-sed -i 's/\r$//' "all_refs.tsv"
-sed -i 's/\r$//' "uniq_refs.tsv"
+# Convert Windows line endings
+sed -i 's/\r$//' "${sample_name}.filtered.tsv" "${sample_name}.refs.tsv"
+cd ../
 
-# Read the references file and extract the sequences
+# Extract sequences from DB
+cd DB_dir
 while IFS= read -r ref_name; do
-    awk -v RS='>' -v ref="$ref_name" '
-    $1 == ref {
-        print ">"$0;
-        exit;
-    }' murema_DB.fasta > "${ref_name}.fasta"
-done < uniq_refs.tsv
+    awk -v RS='>' -v ref="$ref_name" '$1 == ref { print ">"$0; exit }' murema_DB.fasta > "${ref_name}.fasta"
+done < "../${sample_name}/${sample_name}.refs.tsv"
 
-# Index founded references
+# Index references
 for f in *.fasta; do
-    if [ ! -f "${f%.fasta}_index.1.bt2" ]; then
-        bowtie2-build -f "$f" "${f%.fasta}_index"
-    fi
+    [ ! -f "${f%.fasta}_index.1.bt2" ] && bowtie2-build -f "$f" "${f%.fasta}_index" | tee -a "$log_file"
 done
 cd ../
 
-# Consensus & Grapher
-while IFS= read -r sample_name; do
-    cd "$sample_name"
-    while IFS= read -r ref_name; do
-        bowtie2 --end-to-end --very-sensitive -x "../DB_dir/${ref_name}_index" -1 "../${sample_name}_1.fastq.gz" -2 "../${sample_name}_2.fastq.gz" | samtools sort | samtools view -@ 8 -b -F 4 -q 1 -o "${sample_name}.${ref_name}.sorted.bam"
-        samtools mpileup -A -d 6000000 -B -Q 0 -q 20 --reference ../DB_dir/${ref_name}.fasta $sample_name.$ref_name.sorted.bam | ivar consensus -p $sample_name.$ref_name.consensus -t 0.75
-        {
-            echo ""
-            echo "Finished creating consensus sequence for $sample_name using $ref_name as reference"
-        } | tee -a "$log_file"
-        # Run grapher.py script to generate graphs per refs
-        python3 ../grapher.py "${sample_name}.${ref_name}.sorted.bam" "$sample_name" "$ref_name" "$avg_depth_threshold"
-        if [ $? -ne 0 ]; then
-            {
-                echo ""
-                echo "Error: grapher.py failed for sample $sample_name"
-            } | tee -a "$log_file"
-            cd ../
-            continue
-        fi
-    done < "${sample_name}.refs.tsv"
-    {
-        echo ""
-        echo "Finished creating graphs for $sample_name"
-        echo ""
-        echo "Finished Processing Sample: $sample_name"
-    } | tee -a "$log_file"
-    cd ../
-done < "$samples_file"
+# Consensus & Graphing
+cd "$sample_name"
+while IFS= read -r ref_name; do
+    bowtie2 --end-to-end --very-sensitive -p 8 -x "../DB_dir/${ref_name}_index" -1 "$r1_trimmed" -2 "$r2_trimmed" | \
+    samtools sort | samtools view -@ 8 -b -F 4 -q 1 -o "${sample_name}.${ref_name}.sorted.bam"
 
-{
-    echo ""
-    echo "Script completed successfully." | tee -a "$log_file"
-} | tee -a "$log_file"
+    # Consensus generation
+    if [ -n "$bed_file" ] && [ -f "$bed_file" ]; then
+        echo "Creating consensus sequence with primer trimming using $ref_name" | tee -a "$log_file"
+        samtools index "${sample_name}.${ref_name}.sorted.bam"
+        ivar trim -e -i "${sample_name}.${ref_name}.sorted.bam" -b "${bed_file}" -p "${sample_name}.${ref_name}.primertrim"
+        samtools sort "${sample_name}.${ref_name}.primertrim.bam" -o "${sample_name}.${ref_name}.primertrim.sorted.bam"
+        samtools index "${sample_name}.${ref_name}.primertrim.sorted.bam"
+        samtools mpileup -A -d 6000000 -B -Q 0 -q 20 --reference "../DB_dir/${ref_name}.fasta" "${sample_name}.${ref_name}.primertrim.sorted.bam" | \
+            ivar consensus -p "${sample_name}.${ref_name}.consensus" -t 0.75
+        # Graphing with grapher.py
+        grapher.py "${sample_name}.${ref_name}.primertrim.sorted.bam" "$sample_name" "$ref_name" "$avg_depth_threshold" || { echo "grapher.py failed" | tee -a "$log_file"; exit 1; }
+    else
+        echo "Creating consensus sequence without primer trimming for $ref_name" | tee -a "$log_file"
+        samtools mpileup -A -d 6000000 -B -Q 0 -q 20 --reference "../DB_dir/${ref_name}.fasta" "${sample_name}.${ref_name}.sorted.bam" | \
+            ivar consensus -p "${sample_name}.${ref_name}.consensus" -t 0.75
+        # Graphing with grapher.py
+        grapher.py "${sample_name}.${ref_name}.sorted.bam" "$sample_name" "$ref_name" "$avg_depth_threshold" || { echo "grapher.py failed" | tee -a "$log_file"; exit 1; }
+    fi
+done < "${sample_name}.refs.tsv"
+
+echo "Script completed successfully." | tee -a "$log_file"
+
+cd ../
